f7a98d6b4978e7f0ac845e96f1e3a715eee47b39
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-11 17:23:30 +0200

    Path

---
+++ Sources/CarrrdsUtils/Path.swift
@@ -0,0 +1,15 @@
+//
+//  Path.swift
+//  CarrrdsUtils
+//
+//  Created by Alex Nagy on 11.02.2025.
+//
+
+import Foundation
+
+public struct Path {
+    public struct Firestore {
+        static public let profiles = "profiles"
+        static public let cards = "cards"
+    }
+}
